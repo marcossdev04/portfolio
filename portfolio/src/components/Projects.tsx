2b2a3538db013e21add4eb5876d787d4038dae9e
marcoss <marcosdev04@gmail.com>
2024-05-09 00:41:41 -0300

    projects

--- portfolio/src/components/Projects.tsx
+++ portfolio/src/components/Projects.tsx
@@ -8,7 +8,7 @@ export function Projects() {
             "title": "Vpratas",
             "imageSrc": v,
             "description": "Projeto de ecoomerce feito com o intuito da venda de prata 925, com a utilização de NextJs 13, junto com NextUi e funcionalidades como filtro de pesquisa, dinamyc routes, carrinho de compras!",
-            "skills": ["Next 13", "Tailwind", "Typescript", "React", "Next-Ui"],
+            "skills": ["Next 13", "Tailwind", "Typescript", "React", "Next-Ui", "Zustand", "Dynamic Routes"],
             "demo": "https://vpratas-5fp09aocq-marcossdev04s-projects.vercel.app",
             "source": "https://github.com/marcossdev04/VPRATAS"
         },
@@ -16,7 +16,7 @@ export function Projects() {
             "title": "JWT Login",
             "imageSrc": i,
             "description": "Aplicação feita para apenas usuários com login autenticado com JWT conseguir consumir uma API de filmes, podendo listar, adicionar, editar, e excluir os filmes em questão.",
-            "skills": ["Node", "NestJS", "Typescript", "TypeORM", "Docker", "Redis"],
+            "skills": ["Node", "Typescript", "NestJS", "TypeORM", "Docker", "Redis"],
             "demo": "undef",
             "source": "https://github.com/marcossdev04/nest-authenticator"
         },
