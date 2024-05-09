7f03e99586332adadba2fbed7ed6ea83bb7a36d2
marcoss <marcosdev04@gmail.com>
2024-05-09 00:34:49 -0300

    test2

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
 
-export const inter = Inter({ subsets: ["latin"] });
+// export const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "MRCDEV",
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body>{children}</body>
     </html>
   );
 }
